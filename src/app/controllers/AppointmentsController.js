24527b851b9447d5db0e9916b45ea02bb6da3385
Dieggo Carrilho <dieggopsc@gmail.com>
2019-05-09 16:11:14 -0300

    Agendamento e validação de datas disponiveis

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -1,5 +1,6 @@
 const {
-  User
+  User,
+  Appointment
 } = require('../models')
 class AppointmentsController {
   async create (req, res) {
@@ -7,6 +8,25 @@ class AppointmentsController {
     return res.render('appointments/create', {
       provider
     })
+  }
+  async store (req, res) {
+    const {
+      id
+    } = req.session.user
+    const {
+      provider
+    } = req.params
+    const {
+      date
+    } = req.body
+
+    await Appointment.create({
+      user_id: id,
+      provider_id: provider,
+      date
+    })
+
+    return res.redirect('/app/dashboard')
   }
 }
 
